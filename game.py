03378012e5db21874f2fed0d220111b603556f7c
USER <none>
2024-07-13 15:55:37 +0800

    reset command

--- game.py
+++ game.py
@@ -7,9 +7,13 @@ times = 15
 for i in range(1, times + 1):
     y = eval(input("請猜一個數字: "))
     if x == y:
-        print("猜對了！")
+        print(f"猜對了！共猜了{i}次")
         break
     if i == times:
         print(f"很可惜，正確答案是{x}")
-        break
-    print(f"猜錯了！剩{times-i}次機會")
+    else:
+        if x > y:
+            print("猜高一點")
+        else:
+            print("猜低一點")
+    print(f"剩{times-i}次機會")
